8f67f5426ffe4b7a0c53979a26f83a01bd4a1ff1
Tom Payne <twpayne@gmail.com>
2012-04-19 12:51:24 +0200

    Update to upstream version 1.1.0

--- test/testdata.js
+++ test/testdata.js
@@ -1,6 +1,7 @@
 // a set of points in map XY and Lon/Lat that are supposed to correspond between
 // forward and invers transforms
 Proj4js.defs["EPSG:54003"] = "+proj=mill +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +R_A +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:54008"] = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m  no_defs";
 Proj4js.defs["EPSG:54029"] = "+proj=vandg +lon_0=0 +x_0=0 +y_0=0 +R_A +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
 Proj4js.defs["EPSG:2303X"] = "+proj=utm +zone=30 +ellps=intl +units=m +towgs84=-157.89,-17.16,-78.41,2.118,2.697,-1.434,-1.1097046576093785 +no_defs ";
 Proj4js.defs["EPSG:3035"] = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs";
@@ -10,9 +11,8 @@ Proj4js.defs["EPSG:2958"] = "+proj=utm +zone=17 +ellps=GRS80 +units=m +no_defs";
 Proj4js.defs["EPSG:102018"] = "+proj=gnom +lat_0=90 +lon_0=0 +x_0=6300000 +y_0=6300000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
 Proj4js.defs["ESRI:102026"] = "+proj=eqdc +lat_0=0 +lon_0=0 +lat_1=15 +lat_2=65 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
 Proj4js.defs["EPSG:26986"] = "+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000 +y_0=750000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs";
-
-//need test points for these
-
+Proj4js.defs["EPSG:102067"] = "+title=Krovak +proj=krovak +lat_0=49.5 +lon_0=42.5 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +pm=ferro +units=m +towgs84=570.8,85.7,462.8,4.998,1.587,5.261,3.56 +czech +no_defs";
+Proj4js.defs["EPSG:21781"] = "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs";
 /*
 Proj4js.defs["WKT0"] = 'PROJCS["HARN/WI.SheboyganWI-M",GEOGCS["HARN/WI.LL",DATUM["HARN/WI",SPHEROID["GRS1980",6378137.000,298.25722210],TOWGS84[0.0000,0.0000,0.0000,0.000000,0.000000,0.000000,0.00000000]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Transverse Mercator, Wisconsin County Variation"],PARAMETER["false_easting",79857.760],PARAMETER["false_northing",0.000],PARAMETER["scale_factor",1.000000000000],PARAMETER["central_meridian",-87.55000000000000],PARAMETER["latitude_of_origin",43.26666666670000],PARAMETER["Average Geoid Height (meters)",-34.0200],PARAMETER["Average Elevation (system unit)",182.8800],UNIT["Meter",1.00000000000000]]';
 Proj4js.defs["WKT1"] = 'GEOGCS["WGS84 Lat/Long Degrees -180 ==> +180",DATUM["D_WGS_1984",SPHEROID["World_Geodetic_System_of_1984",6378137,298.257222932867]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]',
@@ -23,10 +23,14 @@ Proj4js.defs["WKT5"] = 'PROJCS["SVY21",GEOGCS["SVY21[WGS84]",DATUM["D_WGS_1984",
 */
 
 Proj4js.testPoints = [
+  {code: 'EPSG:21781',
+    xy: [660389.515487, 185731.630396],
+    ll: [8.23, 46.82]
+  },
   {code: 'EPSG:26986',
     xy: [ 231394.84,902621.11],
     ll: [-71.11881762742996,42.37346263960867]
-  }/*,
+  },
   {code: 'ESRI:102026',
     xy: [3257939.781874, 5459865.918947],
     ll: [45.17578125, 41.923828125]
@@ -35,10 +39,18 @@ Proj4js.testPoints = [
     xy: [2359523.653024, 3280192.180346],
     ll: [21.796875, 28.828125]
   },
+  {code: 'EPSG:54008',
+    xy: [738509.49,5874620.38],
+    ll: [11.0, 53.0]
+  },
+  {code: 'EPSG:3035',
+    xy: [4388138.60, 3321736.46],
+    ll: [11.0, 53.0]
+  },
   {code: 'EPSG:102018',
     xy: [350577.5930806119, 4705857.070634324],
     ll: [-75,46]
-  },
+  }/*,
   {code: 'EPSG:2958',
     xy: [411461.807497, 4700123.744402],
     ll: [-82.07666015625, 42.448388671875]
@@ -46,10 +58,6 @@ Proj4js.testPoints = [
   {code: 'EPSG:54009',
     xy: [3891383.58309223, 6876758.9933288],
     ll: [60,60]
-  },
-  {code: 'EPSG:3035',
-    xy: [4388138.60, 3321736.46],
-    ll: [11.0, 53.0]
   },
   {code: 'EPSG:23030',
     xy: [168035.13,4199884.83,-216.62],
